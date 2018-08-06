2607e1d50a32681c270357ec67d335f79da8602b
eminat9 <minkaturalic@gmail.com>
2018-08-06 22:10:41 +0200

    Zadatak

---
+++ src/Konverzija.java
@@ -0,0 +1,33 @@
+
+public class Konverzija {
+	
+	
+	public int broj;
+
+	public int kgUFunte(int broj){
+	}
+
+
+	public int cunteUKg(int broj){
+	}
+
+
+	public int cmUInce(int broj){
+	}
+
+
+	public int celzijusUFarenhajt(int broj){
+	}
+
+
+	public int farenhatuCelzijus(int broj){
+	}
+
+
+
+	}
+	
+	
+	
+
+}
---
+++ src/Provjera.java
@@ -0,0 +1,30 @@
+
+public class Provjera {
+
+	 public int [] niz;
+
+	public int  najmanji(int [] niz){
+	}
+
+
+	public int  najveci(int [] niz){
+	}
+
+
+	public int  zbir(int [] niz){
+	}
+
+
+	public int  prosjek(int [] niz){
+	}
+
+
+	public int[]  unikatni(int [] niz){
+	}
+
+
+	public int[]  sortiran(int [] niz){
+	}
+	}
+
+
