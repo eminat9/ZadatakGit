06cfa5b42e655f2773e7fa38c712b67ebc0247af
Sead Halilovic <sejkos@hotmail.com>
2018-08-08 12:43:43 +0200

    Implementirane metode za konverzaciju

--- src/Konverzija.java
+++ src/Konverzija.java
@@ -1,33 +1,42 @@
 
 public class Konverzija {
-	
-	
+
 	public int broj;
 
-	public int kgUFunte(int broj){
+	public double kgUFunte(double broj) {
+
+		double kg = broj;
+		double lb = kg * 2.2046;
+
+		return lb;
 	}
 
+	public double funteUKg(double broj) {
+		double lb = broj;
+		double kg = lb / 2.2046;
 
-	public int cunteUKg(int broj){
+		return kg;
 	}
 
+	public double cmUInce(double broj) {
+		double cm = broj;
+		double inch = cm * 0.39;
 
-	public int cmUInce(int broj){
+		return inch;
 	}
 
-
-	public int celzijusUFarenhajt(int broj){
+	public double celzijusUFarenhajt(double broj) {
+		double celzius = broj;
+		double farenhait = (9.0 / 5) * celzius + 32;
+		
+		return farenhait;
 	}
 
-
-	public int farenhatuCelzijus(int broj){
+	public double farenhatuCelzijus(double broj) {
+		double farenhait=broj;
+		double celzius = (farenhait - 32) / 1.8;
+		
+		return celzius;
 	}
-
-
-
-	}
-	
-	
-	
 
 }
